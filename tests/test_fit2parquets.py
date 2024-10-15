e8005d8a1ac5264553093dcb348425046fca24a6
Thomas Camminady <0milieux_member@icloud.com>
2024-10-15 09:36:45 +0200

    Refactor.

--- tests/test_fit2parquets.py
+++ tests/test_fit2parquets.py
@@ -4,7 +4,7 @@ import shutil
 import polars as pl
 import pytest
 
-from fit2parquets import fit2parquets
+from fit2parquets.parser import Parser
 
 FIT_FILES = [
     "tests/data/Rund_um_den_Allendorfer_Bahnhof.fit",
@@ -13,15 +13,20 @@ FIT_FILES = [
 ALTERNATE_PATH = "tests/data2"
 FORMATS = ["parquet", "csv"]
 
+
 @pytest.mark.parametrize("ending", FORMATS)
 @pytest.mark.parametrize("fit_file", FIT_FILES)
 def test_parse_fit_file_in_original_location(fit_file, ending):
-    fit2parquets(fit_file, output_format=ending)
+    Parser.fit2parquets(fit_file, output_format=ending)
 
-    if ending=="parquet":
-        df = pl.read_parquet(fit_file.replace(".fit", "") + "/record_mesgs.parquet")
+    if ending == "parquet":
+        df = pl.read_parquet(
+            fit_file.replace(".fit", "") + f"/record_mesgs.{ending}"
+        )
     else:
-        df = pl.read_csv(fit_file.replace(".fit", "") + "/record_mesgs.csv")
+        df = pl.read_csv(
+            fit_file.replace(".fit", "") + f"/record_mesgs.{ending}"
+        )
 
     assert isinstance(df, pl.DataFrame)
     assert len(df) > 0
@@ -30,18 +35,21 @@ def test_parse_fit_file_in_original_location(fit_file, ending):
 @pytest.mark.parametrize("fit_file", FIT_FILES)
 @pytest.mark.parametrize("ending", FORMATS)
 def test_parse_fit_file_in_modified_location(fit_file, ending):
-    fit2parquets(
+    Parser.fit2parquets(
         fit_file,
         write_to_folder_in_which_fit_file_lives=False,
         alternate_folder_path=f"{ALTERNATE_PATH}/{os.path.basename(fit_file).replace('.fit','')}",
-        output_format = ending
+        output_format=ending,
     )
 
-
-    if ending=="parquet":
-        df = pl.read_parquet(fit_file.replace(".fit", "") + "/record_mesgs.parquet")
+    if ending == "parquet":
+        df = pl.read_parquet(
+            fit_file.replace(".fit", "") + f"/record_mesgs.{ending}"
+        )
     else:
-        df = pl.read_csv(fit_file.replace(".fit", "") + "/record_mesgs.csv")
+        df = pl.read_csv(
+            fit_file.replace(".fit", "") + f"/record_mesgs.{ending}"
+        )
     assert isinstance(df, pl.DataFrame)
     assert len(df) > 0
 
